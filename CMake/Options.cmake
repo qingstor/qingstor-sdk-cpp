2696813ff6c160c8ed69d0fbc24af6ef4c16b239
Jim Huang <jimhuaang@outlook.com>
2019-11-04 16:47:03 +0800

    cmake: add some enhancements.
    1. move options to CMake/Options.cmake
    2. format: add indentation, change space to Tab
    3. bump cmake version in all CMakeLists.txt.

--- CMake/Options.cmake
+++ CMake/Options.cmake
@@ -1,2 +1,8 @@
 OPTION(BUILD_STATICLIB "Set to ON to build libqingstor with static linking." OFF)
 OPTION(BUILD_SHAREDLIB "Set to ON to build libqingstor with shared linking." ON)
+OPTION(BUILD_C_STYLE_INTERFACE "Set to ON to build c style interface." OFF)
+OPTION (INSTALL_SDK_HEADERS "Request installation of headers." ON)
+IF (NOT WIN32)
+	# must also specify with CMAKE_BUILD_TYPE=Release 
+	OPTION (OPTIMIZE_SPACE "Build with size-optimized options to reduce code size." OFF)
+ENDIF()
